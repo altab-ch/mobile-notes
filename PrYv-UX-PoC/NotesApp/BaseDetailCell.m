89ea0343c43ccb7968c6f3df115624d2f5944ed5
Mathieu Knecht <mathieuknecht@MacBook-Pro-de-Mathieu.local>
2014-07-13 18:01:27 +0200

    DetailView : modification of the UI design

--- PrYv-UX-PoC/NotesApp/BaseDetailCell.m
+++ PrYv-UX-PoC/NotesApp/BaseDetailCell.m
@@ -30,9 +30,10 @@
 {
     [super awakeFromNib];
     
-    self.borderView.layer.borderColor = [UIColor colorWithRed:32.0f/255.0f green:169.0f/255.0f blue:215.0f/255.0f alpha:1].CGColor;
-    self.borderView.layer.borderWidth = 2.0f;
-    self.borderView.alpha = 0.0f;
+    //self.borderView.layer.borderColor = [UIColor colorWithRed:32.0f/255.0f green:169.0f/255.0f blue:215.0f/255.0f alpha:1].CGColor;
+    self.borderView.layer.borderColor = [UIColor darkGrayColor].CGColor;
+    self.borderView.layer.borderWidth = 0.5f;
+    //self.borderView.alpha = 0.0f;
 }
 
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated
@@ -45,10 +46,11 @@
 - (void)setIsInEditMode:(BOOL)isInEditMode
 {
     _isInEditMode = isInEditMode;
-    
-    [UIView animateWithDuration:0.2 animations:^{
+    [_borderView.layer setBorderColor:_isInEditMode ?[UIColor colorWithRed:32.0f/255.0f green:169.0f/255.0f blue:215.0f/255.0f alpha:1].CGColor:[UIColor darkGrayColor].CGColor];
+    self.borderView.layer.borderWidth = _isInEditMode ?1:0.5;
+    /*[UIView animateWithDuration:0.2 animations:^{
         self.borderView.alpha = _isInEditMode ? 1.0f : 0.0f;
-    }];
+    }];*/
 }
 
 @end
