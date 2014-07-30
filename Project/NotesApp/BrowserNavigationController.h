4a3af5cfa7050117368d3d995df76549c83dcbed
Mathieu Knecht <mathieuknecht@MacBook-Pro-de-Mathieu.local>
2014-07-30 15:12:04 +0200

    Create BrowserNavigationController / start rotation implementation

---
+++ Project/NotesApp/BrowserNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  BrowserNavigationController.h
+//  NotesApp
+//
+//  Created by Mathieu Knecht on 30.07.14.
+//  Copyright (c) 2014 PrYv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BrowserNavigationController : UINavigationController
+
+@end
