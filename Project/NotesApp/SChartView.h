858102daa3ec25536a1d1c9ef52bd1ca164ecb15
Mathieu Knecht <mathieuknecht@MacBook-Pro-de-Mathieu.local>
2014-09-24 16:42:05 +0200

    SChartView added

---
+++ Project/NotesApp/SChartView.h
@@ -0,0 +1,14 @@
+//
+//  SChartView.h
+//  NotesApp
+//
+//  Created by Mathieu Knecht on 24.09.14.
+//  Copyright (c) 2014 PrYv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <ShinobiCharts/ShinobiCharts.h>
+
+@interface SChartView : ShinobiChart
+
+@end
